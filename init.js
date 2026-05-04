a6d38da97673ac516ad451fdf1e77fe87a3ddc44
Marstorekita <likezelwit@gmail.com>
2026-05-04 23:30:44 +0700

    Update init.js

--- init.js
+++ init.js
@@ -19,32 +19,23 @@ try {
 
 // ========== CONSTANTS ==========
 const TOTAL_LOKET = 6;
-const ROOMS_PER_LOKET = 30;
-const PRE_ROLL_VIDEO_ID = ""; // Kosongkan jika tidak mau pakai iklan
-const PRE_ROLL_DURATION_SEC = 0;
+const ROOMS_PER_LOKET = 30; // Total 180 Room
 
 // ========== GLOBAL STATE ==========
 let currentPage = 'home';
 let selectedMovie = null;
 let currentRoomId = null;
-let player = null; 
-let playerType = 'youtube'; 
-let playerReady = false;
 let roomData = null;
 let dbRoomRef = null;
 let dbChatRef = null;
 let dbPresenceRef = null;
 let myUsername = "User_" + Math.floor(Math.random() * 9000 + 1000);
 let countdownInterval = null;
-let syncInterval = null;
-let verifiedVideoData = null;
-let tempRoomData = null;
+let verifiedEmbedData = null;
 let pendingJoinRoomId = null;
-let isSyncing = false;
-let presenceCleanup = null; 
 let occupiedRooms = {}; 
-let isHost = false; // Track if current user is host
-let isPreRollPlaying = false;
+let isHost = false; 
+let presenceCleanup = null; 
 
 // ========== HELPERS ==========
 function loketRoomToGlobal(loket, room) {
